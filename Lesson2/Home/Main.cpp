a215311b261170a616f9ad94d7c5f79c975980e9
Maciek <Maciek@DESKTOP-B0KQNFC>
2016-09-21 14:41:49 +0200

    Dodanie strumienia wejścia

--- Lesson2/Home/Main.cpp
+++ Lesson2/Home/Main.cpp
@@ -2,8 +2,12 @@
 
 using namespace std;
 
+int input;
+
 int main() {
 	cout << "Hello World! :D" << endl;
+	cout << "Wprowadz liczbe 12: " << endl;
+	cin >> input;
 	system("pause");
 	return 0;
 }
