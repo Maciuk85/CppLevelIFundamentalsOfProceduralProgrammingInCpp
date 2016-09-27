bd7e3331f1164737ed9151bba37c8d60351dcbf8
Maciek <Maciek@DESKTOP-B0KQNFC>
2016-09-27 21:41:55 +0200

    Home

--- Lesson2/Home/Main.cpp
+++ Lesson2/Home/Main.cpp
@@ -1,13 +1,59 @@
 #include <iostream>
-
+#include <string>
+#include <cmath>
 using namespace std;
-
-int input;
+string first("M");
+string name("Sieron");
+short temp(15);
+double temp2(15.4);
+short students(25);
+double weight(65.3);
+double height(178.4);
+int everestHeight(884800000);
+bool post(false); //skrzynka pocztowa pusta/pe³na
+long neurons;
+long double pi;
+int users;
+short chairs(30);
+short& chairsRef(chairs);
+double a, b, c; // a,b - przyprostokatne c - przeciwprostokatna
+enum flag
+{
+	red,
+	blue,
+	white
+};
 
 int main() {
-	cout << "Hello World! :D" << endl;
-	cout << "Wprowadz liczbe 12: " << endl;
-	cin >> input;
+	cout << "1. litera imienia: " << first <<  endl;
+	cout << "Nazwisko: " << name << endl;
+	cout << "Temperatura(tylko calkowita): " << temp << "*C"<< endl;
+	cout << "Tempretarutra: " << temp2 << "*C" << endl;
+	cout << "Liczba uczniow w klasie: " << students << endl;
+	cout << "Waga ciala: " << weight << "kg" << endl;
+	cout << "Wzrost: " << height << "cm" << endl;
+	cout << "Wysyokosc Mont Everestu: " << everestHeight << "cm" << endl << endl;
+	cout << "Liczba krzesel :" << chairs << endl;
+	cout << "Liczba krzesel(Ref): " << chairsRef << endl;
+	cout << "Nowa liczba krzesel: ";
+	cin >> chairs;
+	cout << endl;
+	cout << "Liczba krzesel :" << chairs << endl;
+	cout << "Liczba krzesel(Ref): " << chairsRef << endl;
+	cout << "Nowa liczba krzesel(Ref): ";
+	cin >> chairsRef;
+	cout << endl;
+	cout << "Liczba krzesel :" << chairs << endl;
+	cout << "Liczba krzesel(Ref): " << chairsRef << endl << endl;
+	cout << "Pierwsza przyprostokatna: ";
+	cin >> a;
+	cout << endl;
+	cout << "Druga przyprostokatna: ";
+	cin >> b;
+	cout << endl;
+	cout<< "Przyprostokatne: "<< a << "," << b << endl;
+	c = sqrt(pow(a,2)+pow(b,2));
+	cout << "Przeciwprostokatna: "<< c << endl;
 	system("pause");
 	return 0;
 }
